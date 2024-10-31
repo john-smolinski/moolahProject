fd3a1ef80ed1fad70dfe3474486e67cbfc5af7c8
John Smolinski <john.smolinski@gmail.com>
2024-10-31 15:58:25 -0700

    Adding simple read only controller for providers

---
+++ CodeProject.Server/Controllers/ProvidersController.cs
@@ -0,0 +1,40 @@
+﻿using CodeProject.Server.Context;
+using CodeProject.Server.Models;
+using CodeProject.Server.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvidersController : ControllerBase
+    {
+        private readonly MoolahContext _context;
+
+        public ProvidersController(MoolahContext moolahContext)
+        {
+            _context = moolahContext;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Provider>>> GetAll()
+        {
+            try
+            {
+                var result = await _context.Providers.ToListAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponse
+                {
+                    Message = "An error occurred while fetching providers.",
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    Detail = ex.Message
+                });
+            }
+        }
+    }
+}
