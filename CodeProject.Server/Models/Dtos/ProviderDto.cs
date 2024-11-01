b5cf02641a8a3a8d1dd6b44948986ab825e04729
John Smolinski <john.smolinski@gmail.com>
2024-10-31 17:10:28 -0700

    fixing dto

--- CodeProject.Server/Models/Dtos/ProviderDto.cs
+++ CodeProject.Server/Models/Dtos/ProviderDto.cs
@@ -1,6 +1,10 @@
 ﻿namespace CodeProject.Server.Models.Dtos
 {
+#pragma warning disable CS8618
     public class ProviderDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
+#pragma warning restore
 }
